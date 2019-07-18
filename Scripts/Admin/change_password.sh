2557fa0ac09175004a04a1c6268230ba11e6da96
mahyaj98 <jamshidianm98@gmail.com>
2019-07-18 09:40:15 -0400

    change password works fine

--- Scripts/Admin/change_password.sh
+++ Scripts/Admin/change_password.sh
@@ -5,4 +5,6 @@ newpasswd=$2
 domain=$(grep -i "($username:" < /home/user_domain | cut -d ":" -f 2 | cut -d ")" -f 1)
 email=$(grep -i "($username:" < /home/user_email | cut -d ":" -f 2 | cut -d ")" -f 1)
 volume=$(grep -i "($username:" < /home/user_volume | cut -d ":" -f 2 | cut -d ")" -f 1)
-
+oldpasswd=$(grep -i "($username:" < /home/user_passwd | cut -d ":" -f 2 | cut -d ")" -f 1)
+echo -e "$newpasswd\n$newpasswd" | passwd $username
+sed -i "s|($username,$oldpasswd)|($username,$newpasswd)|g" /home/user_domain
