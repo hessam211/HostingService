999365dadd3760f47f1813eb80db17a68820484e
mahyaj98 <jamshidianm98@gmail.com>
2019-07-19 07:41:13 -0400

    now everything works fine for admin

--- Scripts/Admin/delete_user.sh
+++ Scripts/Admin/delete_user.sh
@@ -2,6 +2,10 @@
 
 userdel -r $1
 domain=$(grep -i "($1:" < /home/user_domain | cut -d ":" -f 2 | cut -d ")" -f 1)
+email=$(grep -i "($1:" < /home/user_email | cut -d ":" -f 2 | cut -d ")" -f 1)
+volume=$(grep -i "($1:" < /home/user_volume | cut -d ":" -f 2 | cut -d ")" -f 1)
+passwd=$(grep -i "($1:" < /home/user_passwd | cut -d ":" -f 2 | cut -d ")" -f 1)
+
 
 
 rm -rf /var/www/$domain
@@ -14,7 +18,16 @@ cat tmp > /etc/hosts
 
 grep -v "^($1:$domain)$" /home/user_domain > tmp
 cat tmp > /home/user_domain
+grep -v "^($1:$email)$" /home/user_email > tmp
+cat tmp > /home/user_domain
+grep -v "^($1:$volume)$" /home/user_volume > tmp
+cat tmp > /home/user_domain
+grep -v "^($1:$passwd)$" /home/user_passwd > tmp
+cat tmp > /home/user_domain
 
 grep -v "^$1$" /etc/vsftpd/user_list > tmp
 cat tmp > /etc/vsftpd/user_list
+
+rm /etc/ssl/$domain.key
+rm /etc/ssl/$domain.crt
 
